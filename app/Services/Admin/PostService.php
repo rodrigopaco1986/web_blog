bf7de41b974a1727566cb03fb9723526dcda0bc8
Rodrigo Paco <rodrigopaco.1986@gmail.com>
2020-09-14 23:41:03 -0400

    added htmlpurifier for posts saved by users

--- app/Services/Admin/PostService.php
+++ app/Services/Admin/PostService.php
@@ -6,12 +6,14 @@ use Illuminate\Http\Request;
 use App\Post;
 use Auth;
 use Carbon\Carbon;
+use Stevebauman\Purify\Facades\Purify;
 
 class PostService
 {
 	public static function store($data)
 	{
 		$data['publication_date'] = ($data['publicated'] ? Carbon::now() : null);
+		$data['description'] = Purify::clean($data['description']);
 		$data['user_id'] = Auth::user()->id;
 
 		Post::create($data);
@@ -20,6 +22,7 @@ class PostService
 	public static function update(Post $post, $data)
 	{
 		$data['publication_date'] = ($data['publicated'] ? Carbon::now() : null);
+		$data['description'] = Purify::clean($data['description']);
 
 		$post->update($data);
 	}
